33eec8ba706a67335c9ad4ed1c5f4a9625fef19a
cyl <1063415880@qq.com>
2022-09-08 11:12:57 +0800

    目录结构调整

---
+++ vue-basic/19-单文件组件/School.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>学校名称: {{ name }}}</h2>
+        <h2>学校地址: {{ address }}}</h2>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "School",
+    data() {
+        return {
+            name: "北京大学",
+            address: "北京"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
