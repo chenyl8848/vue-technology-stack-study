29574b528850c6188415b05a2cb2e37d808c687c
cyl <1063415880@qq.com>
2022-09-26 11:24:58 +0800

    1.todo案例完成

---
+++ vue-test/07-src-TodoList案例/components/MyItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          :checked="todoObj.done"
+          @change="handleCheck(todoObj.id)"
+        />
+        <span>{{ todoObj.title }}</span>
+      </label>
+      <button class="btn btn-danger" @click="handleDelete(todoObj.id)">
+        删除
+      </button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyItem",
+  props: ["todoObj", "checkTodo", "deleteTodo"],
+  data() {
+    return {};
+  },
+  methods: {
+    // 勾选 或者 取消勾选
+    handleCheck(id) {
+      this.checkTodo(id);
+    },
+    // 删除
+    handleDelete(id) {
+      this.deleteTodo(id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover {
+  background-color: #ddd;
+}
+
+li:hover button {
+  display: block;
+}
+</style>
