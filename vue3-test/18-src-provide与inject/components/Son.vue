080dc3464562d9210f8200efa9adb67beaede3d5
cyl <1063415880@qq.com>
2022-10-16 10:30:06 +0800

    1.provide与inject

---
+++ vue3-test/18-src-provide与inject/components/Son.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="son">
+    <h1>我是 Son 组件（孙） {{ car.name }} - {{ car.price }}W</h1>
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+
+export default {
+  name: "Son",
+  setup() {
+    // 注入上代传来的数据
+    let car = inject("car");
+
+    return {
+      car,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.son {
+  background-color: orange;
+  padding: 10px;
+}
+</style>
