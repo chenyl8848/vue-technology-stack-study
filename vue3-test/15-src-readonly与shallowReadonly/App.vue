a7a9b76b4319c2fa7029363f9b2e1f88c9a1edce
cyl <1063415880@qq.com>
2022-10-16 09:46:43 +0800

    1.readonly与shallowReadonly

---
+++ vue3-test/15-src-readonly与shallowReadonly/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- vue3 组件中的模板结构可以没有根标签 -->
+
+  <button @click="isShowDemo = !isShowDemo">切换显示/隐藏</button>
+  <Demo v-if="isShowDemo" />
+
+</template>
+
+<script>
+import { ref } from "vue";
+import Demo from "./components/Demo";
+
+export default {
+  name: "App",
+  components: { Demo },
+  setup() {
+    let isShowDemo = ref(true);
+
+    return {
+      isShowDemo,
+    };
+  },
+};
+</script>
+
+
